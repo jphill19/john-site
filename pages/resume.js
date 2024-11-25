5843a7404ddea87c867141f4c67f4e2b37298991
John Hill <jphill1989@gmail.com>
2024-11-25 13:50:47 -0700

    doing no ssr for the resume component

--- pages/resume.js
+++ pages/resume.js
@@ -1,10 +1,13 @@
 import { Box } from '@chakra-ui/react'
 import EmbedResume from '../components/layouts/embed-resume'
 import Section from '../components/layouts/section'
+import NoSsr from '../components/layouts/no-ssr'
 
 const Resume = () => (
   <Section delay={0.4}>
-    <EmbedResume src="/pdf/resume.pdf"/>
+    <NoSsr>
+      <EmbedResume src="/pdf/resume.pdf" />
+    </NoSsr>
   </Section>
 )
 
